2f0f7da205c08059dcfb5172be45151cd1576014
Sergio-Marq <dinuphoenix2025@gmail.com>
2025-04-18 16:53:59 +0530

    added topanime
    Added the top anime section, with various other type features, should still add cache features and loading features to make it more better at functionality, just fetches the top anime content.

--- src/components/TopAnime.tsx
+++ src/components/TopAnime.tsx
@@ -18,11 +18,24 @@ interface AnimeCard {
 
 function TopAnime() {
   const [animeList, setAnimeList] = useState<AnimeCard[]>([]);
+  const [activeType, setActiveType] = useState("all");
 
   useEffect(() => {
   const fetchData = async () => {
     try {
-      const response = await fetch(`https://api.jikan.moe/v4/top/anime?limit=25`);
+      
+      let url = "https://api.jikan.moe/v4/top/anime?limit=25";
+
+      if (activeType === "airing") {
+          url = "https://api.jikan.moe/v4/top/anime?filter=airing&limit=25";
+        } else if (activeType === "tv") {
+          url = "https://api.jikan.moe/v4/top/anime?type=tv&limit=25";
+        } else if (activeType === "movie") {
+          url = "https://api.jikan.moe/v4/top/anime?type=movie&limit=25";
+        } else if (activeType === "ova") {
+          url = "https://api.jikan.moe/v4/top/anime?type=ova&limit=25";
+        }
+      const response = await fetch(url);
       const data = await response.json();
       setAnimeList(data.data);
     } catch (error) {
@@ -31,13 +44,27 @@ function TopAnime() {
   };
 
   fetchData();
-}, []);
+}, [activeType]);
+ const handleTypeChange = (type: string) => {
+    setActiveType(type);
+  };
 
 
   return (
     <>
         <Navbar />
+        
         <div className="top-anime-list">
+          <div className="top-anime"><h2>Top Anime</h2></div>
+        <nav className="top-anime-type">
+          <div className="top-anime-types">
+            <button className={`type ${activeType === "all" ? "active" : ""}`} onClick={()=>handleTypeChange("all")}>All Anime</button>
+            <button className={`type ${activeType === "airing" ? "active" : ""}`} onClick={()=>handleTypeChange("airing")}>Top Airing</button>
+            <button className={`type ${activeType === "tv" ? "active" : ""}`} onClick={()=>handleTypeChange("tv")}>Top TV series</button>
+            <button className={`type ${activeType === "movie" ? "active" : ""}`} onClick={()=>handleTypeChange("movie")}>Top Movies</button>
+            <button className={`type ${activeType === "ova" ? "active" : ""}`} onClick={()=>handleTypeChange("ova")}>Top Ova</button>
+          </div>
+        </nav>
     {animeList.map((anime, index) => (
     <div key={anime.mal_id} className="anime-row">
       <div className="rank">{index + 1}</div>
@@ -53,7 +80,7 @@ function TopAnime() {
       </div>
 
       <div className="score">
-        ⭐ {anime.score ?? 'N/A'}
+        <span >⭐</span> {anime.score ?? 'N/A'}
       </div>
 
       <div className="your-score">
